4463a2272e9b06fba49ff183fc7b3eb9d86cea51
CryingCoder <112829574+CryingCoder@users.noreply.github.com>
2022-09-26 15:30:13 -0400

    Unit 2 Exercise 1

--- Program.cs
+++ Program.cs
@@ -1,28 +1,34 @@
-﻿while (true)
+﻿Console.WriteLine("Welcome to Grand Circus’ Room Detail Generator!");
+Console.WriteLine();
+
+
+
+Console.WriteLine("Enter Length:" );
+double lengthEntered = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter Width: ");
+double widthEntered = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter Height: ");
+double heightEntered = double.Parse(Console.ReadLine());
+
+double areaCalculated = lengthEntered * widthEntered;
+Console.WriteLine("Area: " +areaCalculated);
+double perimeterCalculated = 2*(lengthEntered + widthEntered);
+Console.WriteLine("Perimeter: " +perimeterCalculated);
+double volCalculated = lengthEntered * widthEntered * heightEntered;
+Console.WriteLine("Volume: " + volCalculated);
+double surfCalculated = 2 * ((lengthEntered * widthEntered) + (heightEntered * lengthEntered) + (heightEntered * widthEntered));
+Console.WriteLine("Surface Area: " + surfCalculated);
+
+
+if (areaCalculated <= 250)
 {
-    Console.WriteLine("Please input the radius of a circle");
-
-    string input = Console.ReadLine();
-    double radius;
-
-    if (!double.TryParse(input, out radius))
-    {
-        Console.WriteLine("Please enter a numerical value");
-        continue;
-    }
-
-    Console.WriteLine("Radius: " + radius);
-    double area = Math.PI * radius * radius;
-    Console.WriteLine("Area: " + area);
-
-
-    Console.WriteLine("Would you like to measure the area of another Circle?(Y/N)");
-    string Response = Console.ReadLine().ToLower();
-    if (Response == "y")
-    {
-        continue;
-    }
-    else
-        Console.WriteLine("Thanks for trying!");
-    break;
+    Console.WriteLine("This is a small-sized room.");
 }
+else if (areaCalculated < 650)
+{
+    Console.WriteLine("This is a medium-sized room.");
+}
+else
+    Console.WriteLine("This is a large-sized room.");
+
+Console.WriteLine("Thank you for using the Room Detail Generator!");
